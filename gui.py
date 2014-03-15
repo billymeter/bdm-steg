17232c260ce0fe7e7aaa8de9a9a49322c1d8f620
rums <mercyfulfate@gmail.com>
2014-03-15 19:14:09 -0400

    added some controls, crazy colors

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import wx
+import wx.lib.filebrowsebutton as filebrowse
 import sys
 import os
 import bdm_steg
@@ -13,26 +14,80 @@ def max_fit(src_size, tgt_size):
 
     return (src_width * ratio, src_height * ratio)
 
+class KeyPanel(wx.Panel):
+    def __init__(self, parent, ID):
+        wx.Panel.__init__(self, parent, ID)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.key_label = wx.StaticText(self, label="Enter key:")
+        self.key_box = wx.TextCtrl(self, value="", style=wx.TE_MULTILINE)
+        self.load_key = wx.Button(self, label="Load", id=wx.ID_OK)
+        self.sizer.AddMany(
+            [(self.key_label, wx.ID_ANY, wx.ALIGN_CENTER),
+            (self.key_box, wx.ID_ANY, wx.EXPAND),
+            (self.load_key, wx.ID_ANY, wx.ALIGN_CENTER)])
+        self.SetSizer(self.sizer)
+
 class LeftPanel(wx.Panel):
     def __init__(self, parent, ID):
-        wx.Window.__init__(self, parent, ID)
+        wx.Panel.__init__(self, parent, ID)
+        self.dirname = ''
+        self.SetBackgroundColour(wx.WHITE)
         self.parent = parent
         self.width, self.height = self.GetSize()
-        self.image = None
-        self.textbox = wx.TextCtrl(self)
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(self.textbox, wx.ID_ANY, wx.EXPAND)
-        self.SetSizer(sizer)
-        self.SetBackgroundColour((0, 0, 0))
+        self.load_image = filebrowse.FileBrowseButton(
+            self, wx.ID_ANY, labelText="Load Image", buttonText="Choose file...",
+            fileMask=("Images (*.bmp,*.gif,*.png,*.jpg)|*.bmp;*.gif;*.png;*.jpg|"
+                      "BMP files (*.bmp)|*.bmp|GIF files (*.gif)|*.gif|"
+                      "PNG files (*.png)|*.png|JPG files (*.jpg)|*.jpg"),
+            changeCallback=self.SendImage(parent.right_panel))
+        self.load_file = filebrowse.FileBrowseButton(
+            self, wx.ID_ANY, labelText="Load Message", buttonText="Choose file...",
+            changeCallback=self.SendImage(parent.right_panel))
+        self.key_panel = KeyPanel(self, wx.ID_ANY)
+
+        self.load_image.SetBackgroundColour((105,210,231))
+        self.load_file.SetBackgroundColour((167,219,216))
+        self.key_panel.SetBackgroundColour((224,228,204))
+
+        self.sizer = wx.GridSizer(rows=3)
+        self.sizer.Add(self.load_image, wx.ID_ANY, wx.EXPAND)
+        self.sizer.Add(self.load_file, wx.ID_ANY, wx.EXPAND)
+        self.sizer.Add(self.key_panel, wx.ID_ANY, wx.EXPAND)
+        self.SetSizer(self.sizer)
+
+    def SendImage(self, tgt_panel):
+        def inner_send(event):
+            image = wx.Image(event.GetString())
+            tgt_panel.DisplayImage(image)
+        return inner_send
+
 
 class RightPanel(wx.Panel):
     def __init__(self, parent, ID):
-        wx.Window.__init__(self, parent, ID)
+        wx.Panel.__init__(self, parent, ID)
+        self.SetBackgroundColour(wx.WHITE)
+        sizer = wx.BoxSizer(wx.VERTICAL)
         self.parent = parent
         self.width, self.height = self.GetSize()
         self.image = None
-        self.SetBackgroundColour((255, 255, 255))
+        wx.EVT_PAINT(self, self.OnPaint)
 
+    def OnPaint(self, evt):
+        dc = wx.PaintDC(self)
+        if self.image:
+            dc.DrawBitmap(self.image.ConvertToBitmap(), 0,0)
+
+    def DisplayImage(self, image):
+        """ Load an image """
+        display_rect = wx.GetClientDisplayRect()
+        if display_rect[2] < image.GetWidth() or display_rect[3] < image.GetHeight():
+            self.parent.Maximize()
+            image = image.Scale(*max_fit(image.GetSize(),
+                                   self.GetSize()),
+                                   quality=wx.IMAGE_QUALITY_HIGH)
+        else:
+            image = image.Scale(*self.GetSize(), quality=wx.IMAGE_QUALITY_HIGH)
+        wx.StaticBitmap(self, -1, wx.BitmapFromImage(image))
 
 class Frame(wx.Frame):
     def __init__(self, parent):
@@ -42,8 +97,11 @@ class Frame(wx.Frame):
         self.width, self.height = 512, 480
         self.SetInitialSize((512,480))
 
-        self.left_panel = LeftPanel(self, wx.ID_ANY)
-        self.right_panel = RightPanel(self, wx.ID_ANY)
+        self.right_panel = RightPanel(parent=self, ID=wx.ID_ANY)
+        self.left_panel = LeftPanel(parent=self, ID=wx.ID_ANY)
+        self.right_panel.SetBackgroundColour((250,105,0))
+        self.left_panel.SetBackgroundColour((224,228,204))
+
         self.SetTitle("bdm-steg")
 
         #initialize menu bar
@@ -71,7 +129,7 @@ class Frame(wx.Frame):
 
         self.Bind(wx.EVT_SIZE, self.OnSize)
 
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer = wx.GridSizer(cols=2)
         self.sizer.Add(self.left_panel, wx.ID_ANY, wx.EXPAND)
         self.sizer.Add(self.right_panel, wx.ID_ANY, wx.EXPAND)
 
@@ -83,31 +141,22 @@ class Frame(wx.Frame):
         self.Destroy()
 
     def OnSize(self, event):
-        if self.right_panel.image:
-            im = wx.ImageFromBitmap(self.right_panel.image)
-            im = im.Scale(*self.right_panel.GetSize(), quality=wx.IMAGE_QUALITY_HIGH)
-            wx.StaticBitmap(self.right_panel, -1, wx.BitmapFromImage(im))
         self.Layout()
 
     def OnOpen(self, event):
         """ Load an image """
-        formats = ("Images (*.bmp,*.gif,*.png,*.jpg)|*.bmp;*.gif;*.png;*.jpg|"
-                   "BMP files (*.bmp)|*.bmp|GIF files (*.gif)|*.gif|"
-                   "PNG files (*.png)|*.png|JPG files (*.jpg)|*.jpg")
-        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", formats, wx.OPEN)
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "",
+                            wildcard=("Images (*.bmp,*.gif,*.png,*.jpg)|"
+                                              "*.bmp;*.gif;*.png;*.jpg|"
+                                      "BMP files (*.bmp)|*.bmp|"
+                                      "GIF files (*.gif)|*.gif|"
+                                      "PNG files (*.png)|*.png|"
+                                      "JPG files (*.jpg)|*.jpg"),
+                            style=wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.filename, self.dirname = dlg.GetFilename(), dlg.GetDirectory()
-            self.right_panel.image = wx.Bitmap(os.path.join(self.dirname, self.filename))
-            im = wx.ImageFromBitmap(self.right_panel.image)
-            display_rect = wx.GetClientDisplayRect()
-            if display_rect[2] < im.GetWidth() or display_rect[3] < im.GetHeight():
-                self.Maximize()
-                im = im.Scale(*max_fit(im.GetSize(),
-                                       self.right_panel.GetSize()),
-                                       quality=wx.IMAGE_QUALITY_HIGH)
-            else:
-                im = im.Scale(*self.right_panel.GetSize(), quality=wx.IMAGE_QUALITY_HIGH)
-            wx.StaticBitmap(self.right_panel, -1, wx.BitmapFromImage(im))
+            bitmap = wx.Image(os.path.join(self.dirname, self.filename))
+            self.right_panel.DisplayImage(bitmap)
         dlg.Destroy()
 
     def Update(self, event):
